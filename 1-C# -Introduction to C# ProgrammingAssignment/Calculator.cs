d8ec8d5bb58caec080e37b2a50e2a1f86838d288
zakia1230 <zakia10parween1423@gmail.com>
2022-04-18 23:26:19 +0530

    Uploading Assignment Files

---
+++ 1-C# -Introduction to C# ProgrammingAssignment/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            int result = 0;
+            Console.WriteLine("Enter First Number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("1 for Addition: ");
+            Console.WriteLine("2 for Subtraction: ");
+            Console.WriteLine("3 for Multiplication: ");
+            Console.WriteLine("4 for Division: ");
+            Console.WriteLine("Enter key to perform opration: ");
+            int opr = Convert.ToInt32(Console.ReadLine());
+
+            switch (opr)
+            {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid Number");
+                    break;
+            }
+            Console.WriteLine("Result: " + result);
+        }
+
+    }
+}
---
+++ 1-C# -Introduction to C# ProgrammingAssignment/ParamArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Assignment1
+{
+    internal class ParamArray
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            int i;
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter the number: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+           // int[] arr = { 12, 15, 5, 10, 20 };
+            int sum = sumOfArray(arr);
+            Console.WriteLine("Sum of array: " + sum);
+        }
+
+        public static int sumOfArray(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+}
