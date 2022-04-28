694fc714afe53eb3416cbba3da594c47c7d573a5
zakia1230 <zakia10parween1423@gmail.com>
2022-04-28 23:16:34 +0530

    All assignment uploaded sucessfully

---
+++ GitandGitHubAssignment/Project_java/src/com/cg/client/Test.java
@@ -0,0 +1,10 @@
+package com.cg.client;
+
+public class Test {
+
+	public static void main (String args[])
+	{
+		System.out.println("This a is a sample class project");
+		System.out.println("This a is a project");
+	}
+}
